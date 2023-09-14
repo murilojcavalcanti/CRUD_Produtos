7d4a0bdafdf61b603dfacd42bd81f48b859cdd49
murilojcavalcanti <josemurilo7@gmail.com>
2023-09-13 23:53:31 -0300

    models criado

---
+++ Data/ProdutoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Desafio_IBID.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Desafio_IBID.Data;
+
+public class ProdutoDbContext:DbContext
+{
+    public ProdutoDbContext(DbContextOptions<ProdutoDbContext> opts): base(opts)
+    {
+        
+    }
+
+    public DbSet<Produto> Produtos { get; set; }
+
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Desafio_IBID.Models
+{
+    public class Produto
+    {
+
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string descrição { get; set; }
+
+        [Required]
+        public decimal Preco { get; set; }
+    }
+}
